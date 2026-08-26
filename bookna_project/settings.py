92a43d77a35815fb06543c73a8d4eafdf43068a4
Peter Chol <peterchol.224@gmail.com>
2026-08-26 00:58:10 -0400

    Update deployment settings

--- bookna_project/settings.py
+++ bookna_project/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m68=x1tnq#9674-nr0opv-o6*ery5_!m1ggts5qr@=60ohtupx'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "temporary-local-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    ".onrender.com",
+    "Peterchol.pythonanywhere.com",
+]
 
 
 # Application definition
@@ -133,4 +138,7 @@ MAILERS = {
 
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'media' 
+
+LOGIN_URL = 'owner_login'
+LOGIN_REDIRECT_URL = 'owner_dashboard'
